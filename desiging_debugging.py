713308c6413ed5b3d1736b68e031bc1bd7c31835
chandra dhiman <chandradhiman13@gmail.com>
2024-05-11 19:18:30 +0200

    new file added

---
+++ desiging_debugging.py
@@ -0,0 +1,16 @@
+#ask the user for thr F
+#convert in to a float()
+
+
+F = float(input("Please enter temp:  "))
+C = (F-32)/1.8
+print("The temperature in celcuis is: ", C)
+
+
+K = float(input("Please enter your weight: "))
+P = float(K*2.2)
+print("Your weight in kg is :", P)
+
+P = float(input("Please enter your weight: "))
+K = float(P/2.2)
+print("Your weight in pound: ", P)
